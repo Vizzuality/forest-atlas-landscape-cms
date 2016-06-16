b28c0ed79eeb1f6254b4c81aafbb5c8f14acdd93
Gerardo <gerardo.pacheco@vizzuality.com>
2016-06-16 17:26:37 +0200

    Feat: React config, eslint, styles

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,16 +1,15 @@
-import { createDevTools } from 'redux-devtools'
-import LogMonitor from 'redux-devtools-log-monitor'
-import DockMonitor from 'redux-devtools-dock-monitor'
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
 
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import React from 'react';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 
-import * as reducers from './reducers'
+import * as reducers from './reducers';
 
 import Home from './views/Home';
 
@@ -18,7 +17,7 @@ const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
     <LogMonitor theme="tomorrow" preserveScrollTop={false} />
   </DockMonitor>
-)
+);
 
 const store = createStore(
   combineReducers({
@@ -29,21 +28,19 @@ const store = createStore(
   compose(
     applyMiddleware(thunk)
   )
-)
+);
 
-const history = syncHistoryWithStore(browserHistory, store)
+const history = syncHistoryWithStore(browserHistory, store);
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Provider store={store}>
-          <Router history={history}>
-            <Route path='/' component={Home} />
-          </Router>
-        </Provider>
-        <DevTools store={store} />
-      </div>
-    );
-  }
-};
+export default function App() {
+  return (
+    <div>
+      <Provider store={store}>
+        <Router history={history}>
+          <Route path={'/'} component={Home} />
+        </Router>
+      </Provider>
+      <DevTools store={store} />
+    </div>
+  );
+}
