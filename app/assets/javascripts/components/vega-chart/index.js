7c0d5e207de086980968504545c5ee6e07677e9f
Gerardo <gerardo.pacheco@vizzuality.com>
2016-06-16 17:33:13 +0200

    Feat: Component - Vega chart

---
+++ app/assets/javascripts/components/vega-chart/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class VegaChart extends React.Component {
+
+  componentDidMount() {
+    this.parseVega();
+  }
+
+  componentDidUpdate() {
+    this.parseVega();
+  }
+
+  getData() {
+    const { data, width, height } = this.props;
+    let dataObj = {};
+
+    if (typeof data === 'object') {
+      dataObj = data;
+    } else if (typeof data === 'string') {
+      dataObj = JSON.parse(data);
+    }
+
+    dataObj.width = width;
+    dataObj.height = height;
+
+    return dataObj;
+  }
+
+  parseVega() {
+    const dataObj = this.getData();
+
+    vg.parse.spec(dataObj, chart => {
+      const chartVis = chart({
+        el: this.refs.vegaChart
+      });
+
+      chartVis.update();
+    });
+  }
+
+  render() {
+    return (
+      <div ref="vegaChart" className="c-vega"></div>
+    );
+  }
+}
+
+VegaChart.propTypes = {
+  data: React.PropTypes.any.isRequired,
+  width: React.PropTypes.number.isRequired,
+  height: React.PropTypes.number.isRequired
+};
+
+export default VegaChart;
