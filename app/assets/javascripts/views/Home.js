3a927452170aa236c566437aeb3c571b42c1cb64
Gerardo <gerardo.pacheco@vizzuality.com>
2016-06-16 17:31:24 +0200

    Feat: Home view

--- app/assets/javascripts/views/Home.js
+++ app/assets/javascripts/views/Home.js
@@ -1,15 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>Home</div>
-    );
-  }
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <div>Home</div>
+  );
+}
