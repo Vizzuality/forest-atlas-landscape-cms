a21361eebe32f9a87dd3a46273743af548580be9
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-09-27 17:24:26 +0100

    Basic map integration

--- app/assets/javascripts/routers/front/MapRouter.js
+++ app/assets/javascripts/routers/front/MapRouter.js
@@ -8,6 +8,9 @@
     },
 
     initialize: function () {
+      // We force the body to use the full width
+      document.querySelector('body').classList.add('-wide');
+
       // Instantiate the common views here for the page, use the "routes" object to instantiate
       // per route views
       new App.View.HeaderView({
