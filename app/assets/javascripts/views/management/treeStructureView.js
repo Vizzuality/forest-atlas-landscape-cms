5813afdb5d1b75e9efc9279bb94123c473f20c6c
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-09-30 16:18:12 +0100

    Implements the drag and drop feature

--- app/assets/javascripts/views/management/treeStructureView.js
+++ app/assets/javascripts/views/management/treeStructureView.js
@@ -67,10 +67,92 @@
       //   });
     },
 
+    /**
+     * Make the tree sortable
+     */
+    _initSortableTree: function () {
+      this.$el.find('.js-tree-root').nestedSortable({
+        listType: 'ul',
+        handle: '.js-handle',
+        items: '.js-draggable',
+        toleranceElement: '.js-handle',
+        maxLevels: 3, // Max nested level is 3
+        isTree: true,
+        placeholder: 'page -placeholder',
+        forcePlaceholderSize: true,
+        errorClass: '-invalid',
+        isAllowed: function (placeholder, placeholderParent) {
+          // We allow the element to be dragged in the root element
+          // This condition needs to be here because placeholderParent is undefined in this case
+          if (placeholder.parent().hasClass('js-tree-root')) return true;
+
+          // We don't want the dragged element to be nested under a "Add page" button
+          if (placeholderParent.hasClass('js-not-nestable')) return false;
+
+          // We don't want the dragged element to be placed after a "Add page" button
+          // The button should be the last element of a list, so we check the position of the placeholder (to not be the last)
+          // There's an exception: if we nest the dragged element inside another which hasn't any nested yet
+          var parentNestedElements = placeholder.parent().children();
+          if (parentNestedElements.index(placeholder) === parentNestedElements.length - 1 &&
+            parentNestedElements.length > 1) {
+            return false;
+          }
+
+          return true;
+        },
+        relocate: function () {
+          this._saveStructure();
+          this.render();
+        }.bind(this)
+      });
+    },
+
+    /**
+     * Normalize an object representing a branch of the tree by converting the string ids
+     * as int ids
+     * NOTE: the method is recursive to normalize its children
+     *
+     * @argument {object} branch
+     * @returns {object} normalized branch
+     */
+    _normalizeData: function (branch) {
+      branch.id = +branch.id; // Quick way to convert to int
+      if (branch.children) {
+        branch.children.forEach(function (subBranch) {
+          this._normalizeData(subBranch);
+        }, this);
+      }
+      return branch;
+    },
+
+    /**
+     * Retrieve the structure of the tree and save it to the server
+     */
+    _saveStructure: function () {
+      // The ids are returned as strings so we need to convert them to ints
+      var structure = this.$el.find('.js-tree-root').nestedSortable('toHierarchy', { startDepthCount: 0 })
+        .map(function (item) {
+          return this._normalizeData(item);
+        }, this);
+
+      // We then replace the whole collection with the new structure
+      this.collection.reset(structure);
+
+      // We finally save the collection
+      // $.ajax({
+      //   url: '',
+      //   type: 'PUT',
+      //   contentType: 'application/json',
+      //   data: JSON.stringify(structure)
+      // });
+    },
+
     render: function () {
       this.$el.html(this.template({
         pages: this.collection.toJSON()
       }));
+
+      this._initSortableTree();
     }
 
   });
