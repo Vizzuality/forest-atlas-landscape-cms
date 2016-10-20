079231d0461fad8e22770f785d3aa2dc2edb9aed
Tiago Santos <santos.tiago@gmail.com>
2016-10-20 12:47:55 +0200

    Small fixes to site creation page 1

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -51,72 +51,11 @@ class Admin::SitesController < AdminController
   # POST /admin/sites
   # POST /admin/sites.json
   def create
-=begin
-    @site = Site.new(site_params)
-
-    respond_to do |format|
-      if @site.save
-        format.html { redirect_to admin_site_setting_path(@site) }
-        format.json { render :show, status: :created, location: @site }
-      else
-        format.html { render :new }
-        format.json { render json: @site.errors, status: :unprocessable_entity }
-      end
-    end
-=end
   end
 
   # PATCH/PUT /admin/sites/1
   # PATCH/PUT /admin/sites/1.json
   def update
-    respond_to do |format|
-      case step
-        when :name
-          @site = Site.new(site_params)
-          if @site.valid?
-            session[:site] = site.attributes
-            redirect_to next_wizard_path
-          else
-            render_wizard
-          end
-        when :users
-        when :style
-        when :settings
-        when :finish
-
-      end
-
-
-
-=begin
-      if @site.update(site_params)
-        format.html {
-          case params['site']['step']
-            when '1'
-              redirect_to admin_site_setting_path(@site)
-            when '2'
-              redirect_to admin_site_user_path(@site)
-            when '3'
-              redirect_to display_admin_site_path(@site), notice: 'Site was successfully created! '
-          end
-        }
-        format.json { render :show, status: :ok, location: @site }
-      else
-        format.html {
-          case params['site']['step']
-            when '1'
-              render :edit
-            when '2'
-              render 'admin/site_settings/show'
-            when '3'
-              render 'admin/site_users/edit'
-          end
-        }
-        format.json { render json: @site.errors, status: :unprocessable_entity }
-      end
-=end
-
-    end
   end
 
   # DELETE /admin/sites/1
