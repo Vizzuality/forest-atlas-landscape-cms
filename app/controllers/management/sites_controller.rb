8560715f5a4b8141952270cd4abf45d56134e856
Tiago Santos <santos.tiago@gmail.com>
2016-10-26 19:56:43 +0200

    Tree structure already updates and deletes

--- app/controllers/management/sites_controller.rb
+++ app/controllers/management/sites_controller.rb
@@ -63,7 +63,7 @@ class Management::SitesController < ManagementController
 
   # Creates a closure tree for a site
   def build_pages_tree
-    tree = Page.where(site_id: @site.id).select(:id, :name, :parent_id, :position, :enabled, :type).hash_tree
+    tree = Page.where(site_id: @site.id).select(:id, :name, :parent_id, :position, :enabled, :content_type).hash_tree
     formatted_tree = format_tree tree.keys.first, tree.values.first
     gon.structure = formatted_tree
   end
@@ -71,7 +71,9 @@ class Management::SitesController < ManagementController
   # Formats the tree to send it to Backbone through gon
   def format_tree(node_key, node_value)
     tree = {id: node_key.id, name: node_key.name, parent_id: node_key.parent_id,
-            position: node_key.position, enabled: node_key.enabled, type: node_key.type}
+            position: node_key.position, enabled: node_key.enabled, content_type: node_key.content_type,
+            disableable: node_key.disableable?, deleteUrl: management_site_page_path(node_key.id),
+            editUrl: edit_management_site_page_path(node_key.id)}
     unless node_value.blank?
       children = []
       node_value.each do |key, value|
