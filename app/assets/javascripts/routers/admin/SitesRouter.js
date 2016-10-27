ea35df332be582384a8bd5d3781b92d8c896a1d0
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-10-27 16:33:25 +0100

    Adds the table component

--- app/assets/javascripts/routers/admin/SitesRouter.js
+++ app/assets/javascripts/routers/admin/SitesRouter.js
@@ -1,6 +1,56 @@
 ((function (App) {
   'use strict';
 
+  // This collection is used to display the table
+  var TableCollection = Backbone.Collection.extend({
+    parse: function (data) {
+      var keys;
+      if (data.length) keys = Object.keys(data[0]);
+
+      return data.map(function (row) {
+        var res = {};
+
+        res.row = keys.map(function (key) {
+          switch (true) {
+            case /enabled/.test(key):
+              return {};
+
+            case /(enable|edit|delete)/.test(key):
+              // eslint-disable-next-line no-shadow
+              var res = {
+                name: null,
+                searchable: false
+              };
+
+              // We need extra attributes when making a put or delete request
+              var extraAttributes = '';
+              var method = row[key].method;
+              if (method === 'delete' || method === 'put') {
+                extraAttributes = 'rel="nofollow" data-method="' + method + '"';
+                if (method === 'delete') extraAttributes += ' data-confirm="Are you sure?"';
+              }
+
+              res.html = '<a href="' + row[key].value + '" class="c-table-action-button -' +
+                key + '" title="' + App.Helper.Utils.toTitleCase(key) + '" ' + extraAttributes + '>' +
+                App.Helper.Utils.toTitleCase(key) + '</a>';
+
+              return res;
+
+            default:
+              return {
+                name: key,
+                value: row[key].value,
+                searchable: row[key].searchable,
+                sortable: row[key].sortable
+              };
+          }
+        });
+
+        return res;
+      });
+    }
+  });
+
   App.Router.AdminSites = Backbone.Router.extend({
 
     routes: {
@@ -17,6 +67,14 @@
           { name: 'Sites', url: '/admin/sites/' },
           { name: 'Users', url: '/admin/users/' }
         ]
+      });
+
+      // We initialize the table
+      new App.View.TableView({
+        el: $('.js-table'),
+        collection: new TableCollection(gon.sites, { parse: true }),
+        tableName: 'List of sites',
+        searchFieldContainer: $('.js-table-search')[0]
       });
     }
   });
