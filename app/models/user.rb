ab40300119bd61b3c7069ca8abc327f75242b306
Tiago Garcia <tiagojsag@gmail.com>
2016-10-27 10:19:24 +0200

    Remove devise leftovers

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
   has_many :user_site_associations
   has_many :sites, through: :user_site_associations
 end
