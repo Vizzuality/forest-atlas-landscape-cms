ae8dd7d54735432a279a8650c9181aeaab2c126f
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-10-28 20:04:18 +0100

    Styles the fourth step (settings)

--- app/assets/javascripts/views/admin/flagColorsView.js
+++ app/assets/javascripts/views/admin/flagColorsView.js
@@ -8,16 +8,17 @@
   App.View.FlagColorsView = Backbone.View.extend({
     className: 'c-flag-colors',
     template: HandlebarsTemplates['admin/flag-colors'],
-    collection: new Collection(
-      window.gon ? gon.colorArray: []
-    ),
+    collection: new Collection(window.gon ? gon.colorArray : []),
+
     defaults: {
-      color: '#000000'
+      color: '#000000',
+      maxColors: 4
     },
+
     events: {
       'click .js-add-color': '_addColor',
       'click .js-remove-color': '_removeColor',
-      'input input': '_updateColor'
+      'change input': '_updateColor'
     },
 
     initialize: function (settings) {
@@ -26,38 +27,54 @@
     },
 
     _addColor: function () {
-      if (this._canAddColor()) {
-        this.collection.push({ color: this.options.color });
-        this.render();
+      // If the user wants to add a black color, we need to detect it
+      var colorInputs = this.el.querySelectorAll('input[type="color"]');
+      if (this._canAddColor() && this.collection.length !== colorInputs.length) {
+        var colorArray = Array.prototype.slice.call(colorInputs);
+        this.collection.push({ color: colorArray[colorArray.length - 1].value });
       }
+
+      this.render();
     },
 
-    /*
-
-     */
     _removeColor: function (e) {
-      var index = $(e.target).data('id'),
-        model = this.collection.at(+index);
+      var index = $(e.target).data('id');
+      var model = this.collection.at(+index);
       this.collection.remove(model);
       this.render();
     },
 
     _updateColor: function (e) {
-      var color = $(e.target).val(),
-        position = $(e.target).data('id');
+      console.log('changed');
+      var color = $(e.target).val();
+      var position = $(e.target).data('id');
+      var model = this.collection.at(position);
 
-      this.collection.at(position).set({ color: color });
-      this.render();
+      if (!model) {
+        this.collection.push({ color: color });
+      } else {
+        model.set({ color: color });
+      }
     },
 
     _canAddColor: function () {
-      return (this.collection.length <= 4);
+      return (this.collection.length < this.options.maxColors);
+    },
+
+    _isAddButtonVisible: function () {
+      return (this.collection.length < this.options.maxColors - 1);
     },
 
     render: function () {
       this.$el.html(this.template({
-        colors: this.collection.toJSON(),
+        colors: this.collection.toJSON()
+          .map(function (color, i) {
+            color.index = i + 1; // Index used by the label
+            return color;
+          }),
+        lastColorIndex: this.collection.toJSON().length + 1, // Index used by the label of the input outside the loop
         addable: this._canAddColor(),
+        addButtonVisible: this._isAddButtonVisible(),
         inputId: window.gon && gon.colorControllerId,
         inputName: window.gon && gon.colorControllerName,
         colorsValue: this.collection.toJSON().reduce(function (eachRes, color) {
