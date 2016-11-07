c1a8151cf9fa9313ddfae7b589cce717f227b6c7
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-11-07 13:18:58 +0000

    Adds the action bar to the site creation/edition

---
+++ app/assets/javascripts/routers/admin/SiteCreationRouter.js
@@ -0,0 +1,50 @@
+((function (App) {
+  'use strict';
+
+  App.Router.AdminSiteCreation = Backbone.Router.extend({
+
+    routes: {
+      '(/)': 'index'
+    },
+
+    initialize: function (params) {
+      this.step = params[0] || null;
+    },
+
+    index: function () {
+      // We execute the code specific to the step
+      if (this.step && this.step.length) {
+        var stepMethod = 'init' + this.step[0].toUpperCase() + this.step.slice(1, this.step.length) + 'Step';
+        if (this[stepMethod]) this[stepMethod]();
+      }
+
+      // We want the hidden continue button of the form clicked when the user clicks the one from the action-bar
+      var continueButton = document.querySelector('.js-continue');
+      var hiddenContinueButton = document.querySelector('.js-continue-original');
+
+      continueButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        hiddenContinueButton.click();
+      });
+    },
+
+    initNameStep: function () {
+      var formattedUrls = (window.gon && window.gon.global) ? gon.global.urlArray : [];
+      formattedUrls = formattedUrls.map(function (url) {
+        return {
+          url: url.host,
+          id: url.id
+        };
+      });
+
+      new App.View.UrlsInputView({
+        el: '.js-urls',
+        collection: new Backbone.Collection(formattedUrls)
+      });
+    },
+
+    initSettingsStep: function () {
+      new App.View.FlagColorsView({ el: '.js-flag-colors' });
+    }
+  });
+})(this.App));
