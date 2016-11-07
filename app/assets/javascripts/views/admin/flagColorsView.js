8a0048aa1f16441c96ea17df1997782747e75b3e
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-11-07 17:27:27 +0000

    Improves the behaviour of the flag color picker

--- app/assets/javascripts/views/admin/flagColorsView.js
+++ app/assets/javascripts/views/admin/flagColorsView.js
@@ -8,11 +8,10 @@
   App.View.FlagColorsView = Backbone.View.extend({
     className: 'c-flag-colors',
     template: HandlebarsTemplates['admin/flag-colors'],
-    collection: new Collection(window.gon ? gon.colorArray : []),
 
     defaults: {
       color: '#000000',
-      maxColors: 4
+      maxColors: 5
     },
 
     events: {
@@ -23,46 +22,58 @@
 
     initialize: function (settings) {
       this.options = Object.assign({}, this.defaults, settings);
+      this.collection = new Collection((window.gon && gon.colorArray) || [this.options.color]);
       this.render();
     },
 
+    /**
+     * Add a new element to the collection with the default color
+     */
     _addColor: function () {
-      // If the user wants to add a black color, we need to detect it
-      var colorInputs = this.el.querySelectorAll('input[type="color"]');
-      if (this._canAddColor() && this.collection.length !== colorInputs.length) {
-        var colorArray = Array.prototype.slice.call(colorInputs);
-        this.collection.push({ color: colorArray[colorArray.length - 1].value });
+      if (this._canAddColor()) {
+        this.collection.push({ color: this.options.color });
+        this.render();
       }
-
-      this.render();
     },
 
+    /**
+     * Remove the color designated by the DOM node
+     * @param {object} e - DOM node designating the color to remove
+     */
     _removeColor: function (e) {
-      var index = $(e.target).data('id');
-      var model = this.collection.at(+index);
-      this.collection.remove(model);
-      this.render();
+      if (this._canRemoveColor()) {
+        var index = $(e.target).data('id');
+        var model = this.collection.at(+index);
+        this.collection.remove(model);
+        this.render();
+      }
     },
 
+    /**
+     * Update the color corresponding to the modified input
+     * @param {object} e - DOM input node
+     */
     _updateColor: function (e) {
-      console.log('changed');
       var color = $(e.target).val();
       var position = $(e.target).data('id');
       var model = this.collection.at(position);
-
-      if (!model) {
-        this.collection.push({ color: color });
-      } else {
-        model.set({ color: color });
-      }
+      model.set({ color: color });
     },
 
+    /**
+     * Return whether the user can add a new color
+     * @returns {boolean} true if can add a color
+     */
     _canAddColor: function () {
       return (this.collection.length < this.options.maxColors);
     },
 
-    _isAddButtonVisible: function () {
-      return (this.collection.length < this.options.maxColors - 1);
+    /**
+     * Return whether the user can remove a color
+     * @returns {boolean} true if can remove a color
+     */
+    _canRemoveColor: function () {
+      return this.collection.length > 1;
     },
 
     render: function () {
@@ -70,11 +81,10 @@
         colors: this.collection.toJSON()
           .map(function (color, i) {
             color.index = i + 1; // Index used by the label
+            color.canRemoveColor = this._canRemoveColor();
             return color;
-          }),
-        lastColorIndex: this.collection.toJSON().length + 1, // Index used by the label of the input outside the loop
-        addable: this._canAddColor(),
-        addButtonVisible: this._isAddButtonVisible(),
+          }, this),
+        addButtonVisible: this._canAddColor(),
         inputId: window.gon && gon.colorControllerId,
         inputName: window.gon && gon.colorControllerName,
         colorsValue: this.collection.toJSON().reduce(function (eachRes, color) {
