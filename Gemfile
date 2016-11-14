00353a38bcc4f7655d85c64f954543cc122635f1
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-11-14 16:41:48 +0000

    Adds a basic map to the analysis dashboard

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'autoprefixer-rails'
 source 'https://rails-assets.org' do
   gem 'rails-assets-d3', '~> 3.5.16'
   gem 'rails-assets-vega', '~> 2.5.2'
-  gem 'rails-assets-leaflet', '0.7.7'
+  gem 'rails-assets-leaflet', '1.0.1'
   gem 'rails-assets-backbone'
   gem 'rails-assets-jquery-ui'
   gem 'rails-assets-fuse.js'
--- app/assets/javascripts/routers/front/DashboardRouter.js
+++ app/assets/javascripts/routers/front/DashboardRouter.js
@@ -13,6 +13,15 @@
       new App.View.HeaderView({
         el: document.querySelector('.js-header')
       });
+
+      // This JS will probably be moved to independent views once the architecture will be refined
+      var map = L.map(document.querySelector('.js-map'), {
+        scrollWheelZoom: false
+      })
+        .setView([40.44, -3.70], 10);
+      L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
+      }).addTo(map);
     },
 
     index: function () {
