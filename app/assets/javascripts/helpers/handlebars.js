7cb9c3c96c675af72cbc1735adec4cf6f7a3e658
Clément Prod'homme <clementprdhomme@users.noreply.github.com>
2016-11-23 10:59:28 +0000

    Feature/tree structure refactor (#93)
    * Decreases the coupling between the tree structure and the structure page
    
    * Removes the sucess hash from the URL
    
    The idea is that we can still detect if the saving was successful, but now, if we reload the page, we don't get the message just because the hash was in it.

--- app/assets/javascripts/helpers/handlebars.js
+++ app/assets/javascripts/helpers/handlebars.js
@@ -1,4 +1,10 @@
 Handlebars.registerHelper('if_eq', function (a, b, opts) {
-  if (a === b) return opts.fn(this);
+  // This code is really tricky: when Handlebars is passed the value null, it replaces it with an empty object.
+  // Nevertheless, any other value is also converted to an object using the constructor Boolean, Number or String
+  var isNullObject = function (obj) {
+    return (obj instanceof Object) && !(obj instanceof Number) && !(obj instanceof Boolean) && !Object.keys(obj).length;
+  };
+
+  if (a === b || (b === null && isNullObject(a))) return opts.fn(this);
   return opts.inverse(this);
 });
