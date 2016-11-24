3909f735cf46c6b8971cc953f6f875c93ab444b6
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-11-24 12:02:45 +0000

    Makes the wysiwyg of the open content page work again

---
+++ app/assets/javascripts/routers/management/OpenContentRouter.js
@@ -0,0 +1,27 @@
+((function (App) {
+  'use strict';
+
+  App.Router.ManagementOpenContentStep = Backbone.Router.extend({
+
+    routes: {
+      '(/)': 'index'
+    },
+
+    initialize: function (params) {
+      this.slug = params[0] || null;
+    },
+
+    index: function () {
+      // We instantiate the wysiwyg editor
+      this.wysiwygView = new App.View.WysiwygView({
+        el: '.js-content',
+        defaultBlocks: ['Title', 'Introduction']
+      });
+
+      $('.js-submit').on('click', function () {
+        if (this.wysiwygView) this.wysiwygView.saveHTML();
+      }.bind(this));
+    }
+
+  });
+})(this.App));
