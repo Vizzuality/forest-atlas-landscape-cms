e9fcbde99c0b2cc712c4e8dee792ee2d0a3922c5
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-12-02 10:00:49 +0000

    Implements and styles the filters step

--- app/assets/javascripts/routers/management/FiltersStepRouter.js
+++ app/assets/javascripts/routers/management/FiltersStepRouter.js
@@ -11,9 +11,34 @@
       this.slug = params[0] || null;
     },
 
+    /**
+     * Return the fields used by the dashboardFiltersView
+     * TODO: most of it needs to be removed when the back end returns
+     * the correct values
+     * @returns {object[]} fields
+     */
+    _getFields: function () {
+      return (window.gon && gon.fields).map(function (field) {
+        if (field.type === 'date') {
+          field.min = new Date(field.min);
+          field.max = new Date(field.max);
+
+          // If the date couldn't be parsed, we just remove the field
+          if (Number.isNaN(field.min.getTime()) || Number.isNaN(field.max.getTime())) {
+            return null;
+          }
+        }
+
+        return field;
+      }).filter(function (field) {
+        return !!field;
+      });
+    },
+
     index: function () {
       var dashboardFiltersView = new App.View.DashboardFiltersView({
-        el: '.js-filters'
+        el: '.js-filters',
+        fields: this._getFields()
       });
 
       $('.js-form').on('submit', function () {
