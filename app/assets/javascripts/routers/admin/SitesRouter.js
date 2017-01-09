d977cdbbb3c137030e0015a0d642e75d08e00a87
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-01-09 16:16:51 +0000

    Adds a dialog notification to delete a site

--- app/assets/javascripts/routers/admin/SitesRouter.js
+++ app/assets/javascripts/routers/admin/SitesRouter.js
@@ -31,7 +31,8 @@
               }
 
               res.html = '<a href="' + row[key].value + '" class="c-table-action-button -' +
-                key + '" title="' + App.Helper.Utils.toTitleCase(key) + '" ' + extraAttributes + '>' +
+                key + ((method === 'delete') ? ' js-confirm' : '') +
+                '" title="' + App.Helper.Utils.toTitleCase(key) + '" ' + extraAttributes + '>' +
                 App.Helper.Utils.toTitleCase(key) + '</a>';
 
               return res;
@@ -75,6 +76,27 @@
         collection: new TableCollection(gon.sites, { parse: true }),
         tableName: 'List of sites',
         searchFieldContainer: $('.js-table-search')[0]
+      });
+
+      // We attach a dialog notification to the delete buttons
+      var dialogNotification = new App.View.NotificationView({
+        type: 'warning',
+        content: 'Are you sure you want to permanently delete this site?',
+        dialogButtons: true,
+        closeable: false
+      });
+
+      $('.js-confirm').on('click', function (e) {
+        e.preventDefault();
+        e.stopPropagation(); // Prevents rails to automatically delete the site
+
+        // Callback executed when the user clicks the continue button
+        dialogNotification.options.continueCallback = function () {
+          $.rails.handleMethod($(e.target));
+        };
+
+        // We show the dialog
+        dialogNotification.show();
       });
     }
   });
