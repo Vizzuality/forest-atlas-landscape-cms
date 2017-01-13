05f2b5bf219e74b1cbca13b2fd3dd3ba97eaf2a5
Tiago Santos <santos.tiago@gmail.com>
2017-01-13 12:33:18 +0100

    Added the list of widgets for the widgets tab

--- app/controllers/management/widgets_controller.rb
+++ app/controllers/management/widgets_controller.rb
@@ -4,7 +4,24 @@ class Management::WidgetsController < ManagementController
   #before_action :set_content_type_variables, only: [:new, :edit]
 
   def index
-    gon.datasets = ''
+    gon_widgets = []
+    begin
+      dataset_ids = []
+      @site.contexts.each do |context|
+        context.context_datasets.each do |dataset|
+          dataset_ids << dataset.dataset_id
+        end
+      end
+      dataset_ids.uniq!
+
+      widgets = Widget.where(dataset_id: dataset_ids)
+      widgets.each do |widget|
+        gon_widgets << {id: widget.id, name: widget.name,
+                         description: widget.description, visualization: widget.visualization}
+      end
+    rescue
+    end
+    gon.widgets = gon.widgets
   end
 
   private
