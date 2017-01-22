d4c6a76bdc1f2fbd95a4056afe12c78669061f93
Tiago Santos <santos.tiago@gmail.com>
2017-01-22 20:43:36 +0100

    Second version of context creation

--- app/controllers/context_steps_controller.rb
+++ app/controllers/context_steps_controller.rb
@@ -1,11 +1,19 @@
 class ContextStepsController < ManagementController
   include Wicked::Wizard
 
-  before_action :build_context
   before_action :steps_names
+  before_action :build_context
+  before_action :check_user_permissions
+  before_action :reset_session, only: [:new, :edit]
+  before_action :build_context, only: [:update, :show]
+  before_action :save_context, only: :update
+  before_action :build_steps_data, only: [:update, :show]
 
   steps *Context.form_steps[:pages]
   attr_accessor :steps_names
+
+  CONTINUE = 'CONTINUE'.freeze
+  SAVE = 'SAVE CHANGES'.freeze
 
   # Reset the session
   def new
@@ -22,23 +30,92 @@ class ContextStepsController < ManagementController
   end
 
   def update
-    if @context.valid?
-      redirect_to next_wizard_path
-    else
-      render_wizard
-    end
+    move_forward
   end
 
   private
   def context_params
-    params.require(:context).permit(:id, dataset_ids: [], user_ids: [], site_ids: [] )
+    params.require(:context).permit(:id, :name, dataset_ids: [], user_ids: [], site_ids: [] )
+  end
+
+  def reset_context
+    session[:context] = {}
   end
 
   def build_context
-    @context = Context.new
+    if params[:context_id]
+      @context = Context.find(params[:context_id])
+    else
+      @context = Context.new
+    end
+    @context.form_step = step
+    @context.assign_attributes session[:context] if session[:context]
+    @context.assign_attributes context_params if params[:context]
+  end
+
+  def save_context
+    session[:context] = @context.attributes
   end
 
   def steps_names
     self.steps_names = *Context.form_steps[:names]
+  end
+
+  def check_user_permissions
+    @current_user = current_user
+    return if @current_user.admin
+    if @context.id
+      unless @context.context_owners.include? @current_user
+        redirect_to context_path, notice: 'You do not have permissions to edit this context'
+      end
+    end
+  end
+
+  def save_button?
+    return false unless params[:button]
+    return params[:button].upcase == SAVE.upcase
+  end
+
+  # Saves the current state and goes to the next step
+  # Params:
+  # +next_step_name+:: Next step on pressing continue
+  # +save_step_name+:: Next step on pressing save
+  def move_forward(next_step_name = next_step,
+                   save_step_name = Wicked::FINISH_STEP)
+
+    if save_button?
+      notice_text = @page.id ? 'saved' : 'created'
+      if @context.save
+        redirect_to wizard_path(save_step_name), notice: 'Context successfully ' + notice_text
+      else
+        render_wizard
+      end
+    else # Continue button
+      if @context.valid?
+        redirect_to wizard_path(next_step_name)
+      else
+        render_wizard
+      end
+    end
+  end
+
+  # Creates a list of sites available for that user
+  def permitted_sites
+    @permitted_sites =
+      if @current_user.admin
+        Site.all
+      else
+        @current_user.sites
+      end
+  end
+
+  # Created a list of users that are not admins
+  def permitted_users
+    @permitted_users = User.where(admin: false)
+  end
+
+  def build_steps_data
+    permitted_sites if step == 'sites'
+    permitted_users if step == 'users'
   end
 end
--- app/models/context.rb
+++ app/models/context.rb
@@ -11,17 +11,17 @@
 class Context < ApplicationRecord
   has_many :context_datasets
   has_many :dataset_settings
-
   has_many :context_users
   has_many :users, through: :context_users
   has_many :context_owners, -> {owner}, class_name: 'ContextUser'
   has_many :owners, source: :user, through: :context_owners
   has_many :context_writers, -> {writer}, class_name: 'ContextUser'
   has_many :writers, source: :user, through: :context_writers
-
-
   has_many :context_sites
   has_many :sites, through: :context_sites
+
+  validate :steps_validation
+
 
   cattr_accessor :form_steps do
     { pages: %w[title sites users datasets],
@@ -42,5 +42,26 @@ class Context < ApplicationRecord
       return true if context_dataset.is_dataset_default_context
     end
     return false
+  end
+
+  private
+  def steps_validation
+    step_index = form_steps[:pages].index(form_step)
+
+    if self.form_steps[:pages].index('title') <= step_index
+      self.errors['name'] << 'You must enter a name for the context' if self.name.blank? || self.name.strip.blank?
+    end
+
+    if self.form_steps[:pages].index('sites') <= step_index
+      self.errors['sites'] << 'You must select at least one site' if self.sites.length <= 0
+    end
+
+    # No validations for now
+    if self.form_steps[:pages].index('users') <= step_index
+    end
+
+    # Not validations for now
+    if self.form_steps[:pages].index('datasets') <= step_index
+    end
   end
 end
