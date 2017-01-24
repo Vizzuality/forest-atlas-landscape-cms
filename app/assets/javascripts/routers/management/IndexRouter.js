2435035fd9c1cc8af2203053043e8ff0840cb075
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-01-24 09:52:49 +0000

    Adds warning in the Index routers to not delete them

--- app/assets/javascripts/routers/management/IndexRouter.js
+++ app/assets/javascripts/routers/management/IndexRouter.js
@@ -1,14 +1,16 @@
 ((function (App) {
   'use strict';
 
+  // DO NOT DELETE THIS ROUTER
+  // It is used to instantiate (indirectly through the dispatcher)
+  // the quick links component
+
   App.Router.ManagementIndex = Backbone.Router.extend({
 
     routes: {
       '(/)': 'index'
     },
     index: function () {
-      // We remove the site switcher container
-      $('.js-site-switcher').remove();
     }
 
   });
