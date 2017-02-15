4b8433b9734455f263a171a8b8678a0ee54298da
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-02-15 15:33:30 +0100

    helper methods for session manipulation

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -40,4 +40,14 @@ class ManagementController < ActionController::Base
 
     gon.global.user = { 'name' => current_user.name, 'profile' => edit_management_profile_path(current_user.id), 'logout' => auth_logout_url } if current_user
   end
+
+  # assumes session keys are organised by id, as in session[:page][@page_id]
+  def reset_session_key(key, id, default_value = nil)
+    session[key][id] = default_value
+  end
+
+  # assumes session keys are organised by id, as in session[:page][@page_id]
+  def delete_session_key(key, id)
+    session[key].delete(id)
+  end
 end
