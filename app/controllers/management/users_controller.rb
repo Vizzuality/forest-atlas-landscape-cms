733361d6460b9e88a53840e2586494a0629eb645
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-03-10 12:03:02 +0000

    Allow manager to add new user

---
+++ app/controllers/management/users_controller.rb
@@ -0,0 +1,64 @@
+class Management::UsersController < ManagementController
+  before_filter :load_site
+
+  def new
+    @user = User.new
+    @user.user_site_associations.build(site: @site)
+  end
+
+  def create
+    email = user_params[:email]
+    role = user_params.delete(:site_role)
+    @user = User.find_by_email(email) if email.present?
+    if @user
+      grant_access(role)
+    else
+      create_and_grant_access(role)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :site_role
+    )
+  end
+
+  def load_site
+    @site = Site.find_by(slug: params[:site_slug])
+  end
+
+  def grant_access(role)
+    usa = @user.user_site_associations.find_by_site_id(@site.id)
+    if usa
+      usa.role = role unless usa.role == UserType::MANAGER
+    else
+      @user.user_site_associations.build(site: @site, role: role)
+    end
+    if @user.save
+      redirect_to management_site_site_pages_url, notice: 'User granted access successfully'
+    else
+      render :new
+    end
+  end
+
+  def create_and_grant_access(role)
+    @user = User.new(user_params)
+    @user.user_site_associations.build(site: @site, role: role)
+    if @user.valid?
+      api_response = @user.send_to_api(session[:user_token], management_url)
+      if api_response[:valid]
+        @user.save
+        redirect_to management_site_site_pages_url, notice: 'User created successfully'
+      else
+        @user.errors['id'] << 'API error: ' + api_response[:error].to_s
+        render :new
+      end
+    else
+      render :new
+    end
+  end
+end
