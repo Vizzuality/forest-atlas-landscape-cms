42e3a8f1940a08beed59b825f09c13763bcadab8
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2017-03-30 12:32:50 +0100

    Added debug statements

--- app/router/dynamic_router.rb
+++ app/router/dynamic_router.rb
@@ -7,6 +7,7 @@ class DynamicRouter
   )
 
   def self.reload
+    Rails.logger.debug ">>> DynamicRouter.reload"
     Rails.application.routes_reloader.reload!
   end
 
@@ -17,6 +18,7 @@ class DynamicRouter
       return
     else
 
+    Rails.logger.debug ">>> DynamicRouter.load"
       if @route_cache.empty?
         return unless ActiveRecord::Base.connection.schema_cache.data_source_exists? 'pages'
 
@@ -32,6 +34,7 @@ class DynamicRouter
   end
 
   def self.update_routes_for_site_page(site_page)
+    Rails.logger.debug ">>> DynamicRouter.update_routes_for_site_page #{site_page.try(:id) || 'NULL'}"
     return if site_page.id.nil?
 
     @route_cache.remove('p:'+site_page.id.to_s)
@@ -44,6 +47,7 @@ class DynamicRouter
   end
 
   def self.update_routes_for_site(site)
+    Rails.logger.debug ">>> DynamicRouter.update_routes_for_site #{site.try(:id) || 'NULL'}"
     return if site.id.nil?
 
     @route_cache.remove('s:'+site.id.to_s)
@@ -58,6 +62,7 @@ class DynamicRouter
   end
 
   def self.update_routes_for_route(route)
+    Rails.logger.debug ">>> DynamicRouter.update_routes_for_route #{route.try(:id) || 'NULL'}"
     return if route.id.nil?
 
     @route_cache.remove('r:'+route.id.to_s)
@@ -72,6 +77,7 @@ class DynamicRouter
   def self._build_routes_for_page_and_route(site_page, route)
     return unless ActiveRecord::Base.connection.schema_cache.data_source_exists? 'pages'
 
+    Rails.logger.debug ">>> DynamicRouter._build_routes_for_page_and_route #{site_page.id}, #{route.id}"
     constraints = {}
     constraints.store(:host, route.host.gsub(/https?:\/\//, '')) unless route.blank? || route.host.blank?
 
@@ -117,12 +123,14 @@ class DynamicRouter
   end
 
   def self._declare_route(route)
+    Rails.logger.debug ">>> DynamicRouter._declare_route " + route.inspect
     Rails.application.routes.draw do
       get route.path, :to => route.to, defaults: route.defaults, constraints: route.constraints
     end
   end
 
   def self._load_routes_from_cache
+    Rails.logger.debug ">>> DynamicRouter._load_routes_from_cache"
     @route_cache.all.each do |route|
       _declare_route(route)
     end
