1097fa07e797f241f500e5775d05f341711f088a
Rodrigo <sorodrigo@users.noreply.github.com>
2017-04-21 15:32:12 +0200

    only listen to filter changes if there are filters (#220)

--- app/assets/javascripts/routers/front/DashboardRouter.js
+++ app/assets/javascripts/routers/front/DashboardRouter.js
@@ -99,21 +99,23 @@
       }
 
       // When the dataset is filtered, we need to update the components
-      this.listenTo(this.filters, 'dataset:change', function (dataset) {
-        this.filteredDataset = dataset;
+      if (gon && gon.analysisUserFilters && gon.analysisUserFilters.length) {
+        this.listenTo(this.filters, 'dataset:change', function (dataset) {
+          this.filteredDataset = dataset;
 
-        // eslint-disable-next-line no-shadow
-        for (var i = 0, j = this.options.widgetsCount; i < j; i++) {
-          if (this['widget' + i]) {
-            this['widget' + i].options.data = this._getDataset();
-            this['widget' + i].render();
+          // eslint-disable-next-line no-shadow
+          for (var i = 0, j = this.options.widgetsCount; i < j; i++) {
+            if (this['widget' + i]) {
+              this['widget' + i].options.data = this._getDataset();
+              this['widget' + i].render();
+            }
           }
-        }
 
-        if (this.table) {
-          this._initTable();
-        }
-      });
+          if (this.table) {
+            this._initTable();
+          }
+        });
+      }
     },
 
     /**
