d4e965e3efdf9be2c30d629a0d44933caa58ce41
Tiago Garcia <tiagojsag@gmail.com>
2017-06-01 14:41:04 +0200

    Disable turbolinks on map pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
 
   def page_link(page)
     href = page.content_type.equal?(ContentType::LINK) ? page.content['url'] : page.url
+    no_turbolinks = 'data-turbolinks="false"' if page.content_type.equal?(ContentType::MAP) or page.content_type.equal?(ContentType::LINK)
     target_blank = 'target="_blank"' if page.content_type.equal?(ContentType::LINK) and page.content['target_blank'].eql? '1'
-    "<a href=\"#{ href }\" #{ target_blank }>#{ page.name }</a>".html_safe
+    "<a href=\"#{ href }\" #{ target_blank } #{ no_turbolinks } >#{ page.name }</a>".html_safe
   end
 end
