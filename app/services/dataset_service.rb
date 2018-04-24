39661f637617179c739c1f2a9099fe9b7e4884f4
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-04-24 16:58:21 +0100

    Don't append "/v1" to the API_URL env variable

--- app/services/dataset_service.rb
+++ app/services/dataset_service.rb
@@ -1,7 +1,7 @@
 class DatasetService
   include DatasetFieldsHelper
 
-  @conn ||= Faraday.new(:url => ENV.fetch("API_URL") + '/v1') do |faraday|
+  @conn ||= Faraday.new(:url => ENV.fetch("API_URL")) do |faraday|
     faraday.request :url_encoded
     faraday.response :logger, Rails.logger #, bodies: true # Activate this only for specific debugging
     faraday.adapter Faraday.default_adapter
