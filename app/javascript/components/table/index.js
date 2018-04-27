41a61d005915636be9224c4da27451182999fc5b
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-04-27 18:37:49 +0100

    Fix the table component

--- app/javascript/components/table/index.js
+++ app/javascript/components/table/index.js
@@ -7,8 +7,6 @@ import TableActions from 'components/table/TableActions';
 import Toolbar from 'components/table/Toolbar';
 import TableFooter from 'components/table/TableFooter';
 
-import { Modal } from 'components';
-
 const fuseOptions = {
   shouldSort: true,
   threshold: 0.6,
@@ -26,34 +24,75 @@ class Table extends React.Component {
 
     this.state = {
       q: '',
-      sort: 'asc',
-      modalOpen: false,
-      datasetInfo: null,
+      sort: {
+        /**
+         * Sort order
+         * 1 for ASC, -1 for DESC
+         * @type {1|-1} order
+         */
+        order: 1,
+        /**
+         * Column index used to sort the tbale
+         * @type {number} columnIndex
+         */
+        columnIndex: 0
+      },
       columns: cols,
       // when / if we get dynamic pagination, use that information instead
       pagination: {
-        limit: props.limit || 10,
+        limit: props.limit,
         page: 1,
         offset: 0,
-        pages: parseInt(props.data.length / (props.limit || 10))
+        pages: Math.ceil(props.data.length / props.limit)
       }
     };
 
     this.fuse = new Fuse(props.data, { keys: cols.map(col => `${col.toLowerCase()}.value`), ...fuseOptions });
   }
 
-  onSearch(e) {
-    this.setState({ q: e.target.value });
+  /**
+   * Event handler executed when the user
+   * sorts a column
+   * @param {string} column Column
+   */
+  onSort(column) {
+    const columnIndex = this.state.columns.indexOf(column);
+    if (columnIndex !== -1) {
+      if (columnIndex === this.state.sort.columnIndex) {
+        this.setState({ sort: { ...this.state.sort, order: this.state.sort.order * -1 } });
+      } else {
+        this.setState({ sort: {
+          ...this.state.sort,
+          columnIndex,
+          order: 1
+        } });
+      }
+
+      this.setState({ pagination: { ...this.state.pagination, page: 1, offset: 0 } });
+    }
   }
 
-
-  onCloseModal() {
-    this.setState({ modalOpen: false, datasetInfo: null });
+  onSearch(e) {
+    this.setState({
+      q: e.target.value,
+      pagination: {
+        ...this.state.pagination,
+        offset: 0,
+        page: 1
+      }
+    });
   }
 
   setRowsPerPage(e) {
-    const pagination = { ...this.state.pagination, limit: parseInt(e.target.value) };
-    this.setState({ pagination })
+    const pagination = {
+      ...this.state.pagination,
+      limit: parseInt(e.target.value),
+      pages: Math.ceil(this.props.data.length / parseInt(e.target.value)),
+      page: 1,
+      offset: 0
+    };
+
+    this.setState({ pagination });
   }
 
   offsetPage(p) {
@@ -65,16 +104,12 @@ class Table extends React.Component {
     this.setState({ pagination });
   }
 
-  showRowInfo(data) {
-    this.setState({ modalOpen: true, datasetInfo: data });
-  }
-
-  formatRow(d, k) {
+  formatRow(d) {
     const { actions } = this.props;
     return (
-      <tr role="row" key={k}>
+      <tr role="row" key={d}>
         {this.formatCols(d)}
-        {actions && <TableActions actions={actions} data={d} showRowInfo={i => this.showRowInfo(i)} />}
+        {actions && <TableActions actions={actions} data={d} onClickAction={this.props.onClickAction} />}
       </tr>
     );
   }
@@ -138,36 +173,88 @@ class Table extends React.Component {
     });
   }
 
-  render() {
-    const { data, actions, modal, searchable } = this.props;
-    const { q, sort, columns, pagination, modalOpen, datasetInfo } = this.state;
+  /**
+   * Return the sorted rows
+   * @param {Row[]} rows Rows
+   * @returns {Row[]}
+   */
+  sortResults(rows) {
+    return rows.sort((a, b) => {
+      const column = this.state.columns[this.state.sort.columnIndex].toLowerCase();
 
-    const filteredResults = (q.length > 0 && searchable) ? this.fuse.search(q) : data;
+      const cellA = a[column];
+      const cellB = b[column];
+
+      const valA = Array.isArray(cellA.value) ? cellA.value[0] : cellA.value;
+      const valB = Array.isArray(cellB.value) ? cellB.value[0] : cellB.value;
+
+      if ((valA === undefined || valA === null) && (valB === undefined || valB === null)) {
+        return 0;
+      } else if ((valA === undefined || valB === null) && valB) {
+        return -1 * this.state.sort.order;
+      } else if (valA && (valB === undefined || valB === null)) {
+        return this.state.sort.order;
+      }
+
+      if (typeof valA === 'number' && typeof valB === 'number') {
+        if (valA < valB) return -1 * this.state.sort.order;
+        if (valA > valB) return this.state.sort.order;
+        return 0;
+      } else if (typeof valA === 'number' && typeof valB !== 'number') {
+        return -1 * this.state.sort.order;
+      } else if (typeof valA !== 'number' && typeof valB === 'number') {
+        return this.state.sort.order;
+      }
+
+      return valA.localeCompare(valB, [], { sensitivity: 'base' }) * this.state.sort.order;
+    });
+  }
+
+  render() {
+    const { data, actions, searchable } = this.props;
+    const { q, sort, columns, pagination } = this.state;
+
+    const filteredResults = this.sortResults(q.length > 0 && searchable
+      ? this.fuse.search(q)
+      : data);
+
+    const sortName = sort.order === 1 ? 'ascending' : 'descending';
+    const isSortable = (column) => {
+      if (data.length && data[0][column.toLowerCase()]) {
+        return !!data[0][column.toLowerCase()].sortable;
+      }
+
+      return false;
+    };
 
     return (
       <div className="c-table">
         {searchable && <Toolbar q={q} onSearch={query => this.onSearch(query)} />}
         <table role="grid">
+          <caption>
+            {this.props.name}, sorted by {columns[sort.columnIndex]}: {sortName}
+          </caption>
           <thead>
             {filteredResults.length > 0 &&
             <tr className="header" role="row">
                 {columns.map((col, k) => (
                   <th
-                    key={k}
-                    {...(k === 0 ?
-                    { className: `-order-${sort === 'asc' ? 'ascending' : 'descending'}` } : {}
-                    )}
+                    key={col}
+                    aria-sort={k === sort.columnIndex ? sortName : 'none'}
+                    tabIndex={k === sort.columnIndex ? '0' : '-1'}
+                    className={k === sort.columnIndex ? `-order-${sortName}` : ''}
                     role="columnheader"
+                    onClick={() => isSortable(col) && this.onSort(col)}
                   >{col}
                   </th>))}
                 {/* Render empty rows for each action */}
-                {actions && actions.map((a, k) => (<th key={k} aria-sort="none" role="columnheader" />))}
+                {actions && actions.map(a => (<th key={a} aria-sort="none" role="columnheader" />))}
             </tr>
             }
           </thead>
           <tbody>
             {filteredResults &&
-              filteredResults.map((d, k) => (this.verifyPagination(k) ? this.formatRow(d, k) : null))}
+              filteredResults.map((d, k) => (this.verifyPagination(k) ? this.formatRow(d) : null))}
 
             {filteredResults.length === 0 &&
             <tr role="row">
@@ -179,24 +266,85 @@ class Table extends React.Component {
         </table>
 
         <TableFooter
-          pagination={pagination}
+          pagination={{
+            ...pagination,
+            pages: filteredResults
+              ? Math.ceil(filteredResults.length / pagination.limit)
+              : pagination.pages
+          }}
           offsetPage={p => this.offsetPage(p)}
           setRowsPerPage={e => this.setRowsPerPage(e)}
         />
-
-        <Modal show={modalOpen} onClose={() => this.onCloseModal()}>
-          {modal && typeof modal === 'function' ? React.createElement(modal, datasetInfo) : null}
-        </Modal>
       </div>
     );
   }
 }
 
+/* eslint-disable max-len */
+/**
+ * Definition of a row
+ * @typedef {{ [column: string]: { value?: any, searchable?: boolean, sortable?: boolean, link?: { url: string, external: boolean }, method?: string } }}  Row
+ */
+/* eslint-enable max-len */
+
 Table.propTypes = {
+  /**
+   * Name of the table (for screen readers)
+   */
+  name: PropTypes.string.isRequired,
+  /**
+   * Number of results per page
+   */
   limit: PropTypes.number,
-  data: PropTypes.array.isRequired,
-  columns: PropTypes.array.isRequired,
-  actions: PropTypes.array.isRequired
+  /**
+   * Whether the user can search text in the table
+   */
+  searchable: PropTypes.bool,
+  /**
+   * Data to display in the table
+   * Each row contains several columns with their value and
+   * an attribute to tell if the column can be sortable
+   * NOTE: The name of the columns must be in lowercase
+   * NOTE: the data will always be sorted ASC by the first column
+   * The value attribute can have an array of strings
+   * An optional link attribute can be present, it contains its url and an attribute to tell
+   * if the link is external or not (this is not built for multi-value cells)
+   * An example of the format can be:
+   * [
+   *   {
+   *     name: { value: '$3' },
+   *     description: { value: ['Spain', 'France'], sortable: false },
+   *     price: { value: 'iPhone 6', link: { url: 'https://www.apple.com', external: true }, searchable: false }
+   *   }
+   * ]
+   * @type {Row[]} data
+   */
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  /**
+   * Name of the columns
+   * Make sure the name matches one attribute of the
+   * rows (case insensitive)
+   */
+  columns: PropTypes.arrayOf(PropTypes.string).isRequired,
+  /**
+   * List of actions for each row
+   * @type {'edit'|'delete'|'toggle'|'info'} actions
+   */
+  actions: PropTypes.arrayOf(PropTypes.oneOf(['edit', 'delete', 'toggle', 'info'])),
+  /**
+   * Event handler executed when the user clicks the action
+   * or a row
+   * Gets passed the action and the row
+   * @type {(action: 'edit'|'delete'|'toggle'|'info', row: Row): function} onClickAction
+   */
+  onClickAction: PropTypes.func
+};
+
+Table.defaultProps = {
+  limit: 10,
+  searchable: false,
+  actions: [],
+  onClickAction: () => {}
 };
 
 export default Table;
