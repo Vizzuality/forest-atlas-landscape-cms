ff853a194ca18b468c935a53169abcf333143bd2
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-04-27 18:38:33 +0100

    Configure the table component for the widgets listing

--- app/javascript/pages/management/widgets/WidgetsPage.js
+++ app/javascript/pages/management/widgets/WidgetsPage.js
@@ -3,13 +3,22 @@ import PropTypes from 'prop-types';
 import { Table } from 'components';
 
 function WidgetsPage(props) {
+  const rows = props.widgets.map(widget => ({
+    id: { value: widget.id },
+    name: { value: widget.name, sortable: true },
+    description: { value: widget.description }
+  }));
+
   return (
     <div className="l-page-list">
       <div className="wrapper">
         <Table
-          columns={['Name', 'Description', 'chart type']}
-          data={props.widgets}
+          name="List of widgets"
+          searchable
+          columns={['Name', 'Description']}
+          data={rows}
           actions={['edit', 'delete']}
+          onClickAction={(...params) => console.log(params)}
         />
       </div>
     </div>
