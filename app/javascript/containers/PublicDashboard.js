5eb02b4784d9996ae1948c069fa956a6f1e1ff73
Tomas Eriksson <tomasmoosee@gmail.com>
2018-05-03 15:35:00 +0200

    add widget report mock

--- app/javascript/containers/PublicDashboard.js
+++ app/javascript/containers/PublicDashboard.js
@@ -4,9 +4,16 @@ import PublicContainer from 'containers/shared/PublicContainer';
 
 import Wysiwyg from 'vizz-wysiwyg';
 
+import WidgetReports from 'components/WidgetReports';
+
 export default class PublicDashboard extends PublicContainer {
   render() {
     const { content } = this.props.page;
-    return <Wysiwyg readOnly items={content.length ? content : []} />;
+    return (
+      <div>
+        <Wysiwyg readOnly items={content.length ? content : []} />
+        {/* <WidgetReports /> */}
+      </div>
+    );
   }
 }
