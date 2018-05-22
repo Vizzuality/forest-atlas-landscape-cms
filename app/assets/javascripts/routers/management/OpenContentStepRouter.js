94e66157bb621c9ba7f5242c2071f90b0b9a311e
Tomas Eriksson <tomasmoosee@gmail.com>
2018-05-22 12:43:40 +0200

    dont parse widgets again, will endup in json error

--- app/assets/javascripts/routers/management/OpenContentStepRouter.js
+++ app/assets/javascripts/routers/management/OpenContentStepRouter.js
@@ -9,14 +9,13 @@
 
     initialize: function (params) {
       this.slug = params[0] || null;
-
       if (gon && gon.widgets) {
         this.widgets = gon.widgets.map(function (chart) {
-          var vis = JSON.parse(chart.visualization);
+          var vis = chart.visualization
           if (vis.type !== 'chart') {
             vis = Object.assign({}, vis, { type: 'chart', chart: vis.type});
           }
-          chart.visualization =  JSON.stringify(vis);
+          chart.visualization =  vis ? JSON.stringify(vis) : null;
           return chart;
         });
       }
