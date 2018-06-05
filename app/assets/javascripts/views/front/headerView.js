d4cb32bd27a84db60575ccc042c76bd70e79242c
Tomas Eriksson <tomasmoosee@gmail.com>
2018-06-05 15:06:49 +0200

    add el to header view, check if initialized

--- app/assets/javascripts/views/front/headerView.js
+++ app/assets/javascripts/views/front/headerView.js
@@ -9,9 +9,11 @@
 
     initialize: function () {
       this.drawer = this.el.querySelector('.js-mobile-drawer');
+      this.el.classList.add('initialized');
     },
 
     toggleDrawer: function () {
+      console.log('im toggeling');
       var opened = this.drawer.classList.toggle('-opened');
       var overflow = 'auto';
       if (opened) overflow = 'hidden';
