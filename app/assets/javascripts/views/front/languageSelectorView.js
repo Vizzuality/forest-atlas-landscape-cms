ae67b7f967711209f034d75f87b855d546336b0c
Tomas Eriksson <tomasmoosee@gmail.com>
2018-06-08 10:31:13 +0200

    remove turbolinks logic for language selector

--- app/assets/javascripts/views/front/languageSelectorView.js
+++ app/assets/javascripts/views/front/languageSelectorView.js
@@ -36,16 +36,6 @@
           this._setMapLanguage(currentLanguageCode);
         }
 
-        // If the URL doesn't contain the language query parameter, we add it so it
-        // can be shareable
-        if (!/\?(.*)?l=[a-z]{2}/.test(location.search)) {
-          var url = (!location.search.length ? '?' : '&') + 'l=' + currentLanguageCode;
-          // NOTE: adding { turbolinks: {} } is mandatory to avoid breaking the browser's back button
-          // because Turbolinks doesn't handle well the URL changes
-          // Check here: https://github.com/turbolinks/turbolinks/issues/219
-          history.replaceState({ turbolinks: {} }, '', url);
-        }
-
         this.render();
       }.bind(this));
     },
