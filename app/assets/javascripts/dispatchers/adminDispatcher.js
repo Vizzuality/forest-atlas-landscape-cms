8215885e226c7b845fadd4129308fa768d9b711f
Tomas Eriksson <tomasmoosee@gmail.com>
2018-06-13 10:33:14 +0200

    remove route dispatcher triggering turbolinks

--- app/assets/javascripts/dispatchers/adminDispatcher.js
+++ app/assets/javascripts/dispatchers/adminDispatcher.js
@@ -1,45 +1,10 @@
 (function (App) {
   'use strict';
-
-  var Dispatcher = Backbone.Router.extend({
-    routes: {
-      'users(/)': 'Users',
-      '(sites)(/)': 'Sites',
-      '(map_versions)(/)': 'MapVersions',
-      'sites/:slug/site_steps/:step(/)': 'SiteEdition',
-      'site_steps/(:step)': 'SiteCreation'
-    }
+  // We instantiate the Quick links component
+  new App.View.QuickLinksView({
+    activeLink: 'admin'
   });
 
-  var init = function () {
-    var dispatcher = new Dispatcher();
-
-    dispatcher.on('route', function (routeName, params) {
-      Backbone.history.stop();
-      var Router = App.Router['Admin' + routeName];
-
-      if (Router) {
-        new Router(params.slice(0, params.length - 1));
-
-        Backbone.history.start({ pushState: false });
-      }
-    });
-
-    // Because turbolinks doesn't fully reload the page, we need to stop the
-    // history before anything else
-    Backbone.history.stop();
-
-    // We need this to detect router pathname
-    Backbone.history.start({ pushState: true, root: '/admin' });
-
-    // We instantiate the Quick links component
-    new App.View.QuickLinksView({
-      activeLink: 'admin'
-    });
-
-    // We instantiate the User links component
-    new App.View.UserLinksView();
-  };
-
-
+  // We instantiate the User links component
+  new App.View.UserLinksView();
 }).call(this, this.App);
