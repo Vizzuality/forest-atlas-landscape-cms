60ab384c017f12ae0614a63df7303731ae995661
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-07-19 11:14:20 +0100

    Fix a bug where the new dashboard wouldn't load
    If the user would let one of the wysiwyg (or both) empty, the dashboard wouldn't load.

--- app/javascript/pages/admin/edit-dashboard.js
+++ app/javascript/pages/admin/edit-dashboard.js
@@ -9,7 +9,6 @@ import Dashboard from 'components/shared/Dashboard';
 class EditDashboard extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
     this.state = {
       topContent: props.topContent,
       bottomContent: props.bottomContent
@@ -22,7 +21,7 @@ class EditDashboard extends React.Component {
         <input type="hidden" name="site_page[dashboard_setting][content_top]" value={this.state.topContent || ''} />
         <input type="hidden" name="site_page[dashboard_setting][content_bottom]" value={this.state.bottomContent || ''} />
         <Wysiwyg
-          items={JSON.parse(this.props.topContent) || []}
+          items={this.props.topContent ? JSON.parse(this.props.topContent) : []}
           onChange={content => this.setState({ topContent: JSON.stringify(content) })}
           blocks={{
             image: {
@@ -42,7 +41,7 @@ class EditDashboard extends React.Component {
         />
         <Dashboard preview pageSlug="preview" dataset={this.props.dataset} widget={this.props.widget} />
         <Wysiwyg
-          items={JSON.parse(this.props.bottomContent) || []}
+          items={this.props.bottomContent ? JSON.parse(this.props.bottomContent) : []}
           onChange={content => this.setState({ bottomContent: JSON.stringify(content) })}
           blocks={{
             image: {
