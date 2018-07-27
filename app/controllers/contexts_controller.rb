456162237eaf6d0559281f2bd431db3394355bed
Tiago Santos <tiago.santos@vizzuality.com>
2018-07-27 17:01:27 +0100

    Fix: the list of writers/readers of the contexts now have no repeated items

--- app/controllers/contexts_controller.rb
+++ app/controllers/contexts_controller.rb
@@ -34,8 +34,8 @@ class ContextsController < ManagementController
         'name' => {'value' => context.name, 'searchable' => true, 'sortable' => true},
         'sites' => {'value' => context.sites.map{|s| s.name}, 'searchable' => true, 'sortable' => true},
         'datasets' => {'value' => datasets, 'searchable' => true, 'sortable' => true},
-        'owners' =>  {'value' => context.owners.map{|o| o.name}, 'searchable' => true, 'sortable' => true},
-        'writers' => {'value' => context.writers.map{|w| w.name}, 'searchable' => true, 'sortable' => true},
+        'owners' =>  {'value' => context.owners.map{|o| o.name}.uniq, 'searchable' => true, 'sortable' => true},
+        'writers' => {'value' => context.writers.map{|w| w.name}.uniq, 'searchable' => true, 'sortable' => true},
         'edit' => {'value' => edit_link, 'method' => 'get'},
         'delete' => {'value' => delete_link, 'method' => 'delete'}
       }
