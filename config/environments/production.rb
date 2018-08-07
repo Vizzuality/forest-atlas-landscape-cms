5783d82d03da42b8df18280384db0dda9e51748c
Tiago Santos <tiago.santos@vizzuality.com>
2018-08-07 11:45:14 +0100

    Added logging to the assets precompilation on capistrano production deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.assets.logger = Logger.new $stdout
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
