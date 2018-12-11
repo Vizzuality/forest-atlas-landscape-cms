0323159ee616881910fc995b2d067d9e37f558f3
Clément Prod'homme <contact@clementprodhomme.fr>
2018-12-11 18:42:33 +0000

    Reset the state of the map form when changing version (creation only)

--- app/assets/javascripts/routers/management/MapStepRouter.js
+++ app/assets/javascripts/routers/management/MapStepRouter.js
@@ -9,11 +9,84 @@
 
     initialize: function (params) {
       this.slug = params[0] || null;
+      this.pageId = params[1] || null;
     },
 
     index: function () {
-      var editor = document.querySelector('.js-editor');
-      CodeMirror.fromTextArea(editor, { mode: 'javascript', json: true });
+      // We instantiate summernote for some textareas
+      this.initTextAreas();
+
+      // The following code is just for the page creation
+      if (this.pageId !== null && this.pageId !== undefined) {
+        return;
+      }
+
+      var versionsSelector = document.querySelector('.js-version');
+      versionsSelector.addEventListener('change', function (e) {
+        var version = e.target.value;
+        var config = _.find(gon.map_versions, function(v) {
+          return v.default_settings && v.default_settings.version === version;
+        });
+
+        if (config) {
+          var values = JSON.parse(config.default_settings.settings);
+          this.setDefaultValues(values);
+        }
+      }.bind(this));
+    },
+
+    initTextAreas: function () {
+      var selector = '#site_page_content_settings_narrative, #site_page_content_settings_alternativeNarrative';
+      $(selector).summernote({
+        toolbar: [
+          ['link'],
+          ['style', ['bold', 'italic', 'underline', 'clear']],
+          ['font', ['strikethrough', 'superscript', 'subscript']],
+          ['para', ['ul', 'ol', 'paragraph']],
+        ]
+      });
+    },
+
+    setDefaultValues: function (values) {
+      var form = document.querySelector('.js-form');
+      var keys = Object.keys(values);
+
+      keys.forEach(function (key) {
+        var input = form.querySelector('[name="site_page[content][settings][' + key + ']"]');
+        if (input) {
+          if (input.tagName === 'INPUT') {
+            if (input.type === 'text') {
+              input.value = values[key];
+            } else if (input.type === 'hidden') {
+              // We assume it's an input associated with a checkbox
+              var sibling = input.nextElementSibling;
+              if (sibling && sibling.matches('input[type="checkbox"]')) {
+                sibling.checked = !!values[key];
+              } else {
+                // We assume it's a field with a JSONEditor instance
+                if (sibling.editor) {
+                  sibling.editor.set(values[key]);
+                }
+              }
+            }
+          } else if (input.tagName === 'TEXTAREA') {
+            // FIXME: summernote fails here and throws an error
+            try {
+              $(input).summernote('reset');
+              $(input).summernote('insertText', values[key]);
+            } catch (e) {
+              console.error(e);
+            }
+          } else if (input.tagName === 'SELECT') {
+            var selectedIndex = _.findIndex(input.options, { value: values[key] });
+            if (selectedIndex !== -1) {
+              input.selectedIndex = selectedIndex;
+            }
+          }
+        }
+      });
+
+      // this.initTextAreas();
     }
 
   });
