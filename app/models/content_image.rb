6fd82d209f4a90fe80e69df9115a68fc3f260ff8
Tiago Santos <santos.tiago@gmail.com>
2018-12-31 10:50:21 +0000

    Converts images from base64 to files

--- app/models/content_image.rb
+++ app/models/content_image.rb
@@ -1,6 +1,15 @@
 class ContentImage < ApplicationRecord
-  belongs_to :site_page
+  belongs_to :site_page, foreign_key: :page_id
 
   has_attached_file :image, styles: { cover: '1280x>', thumb: '110x>' }
   validates_attachment_content_type :image, content_type: %r{^image\/.*}
+
+  def self.create_from_base64(page_id, base64)
+    content_image = ContentImage.new
+    content_image.page_id = page_id
+    image = Paperclip.io_adapters.for(base64)
+    image.original_filename = Time.now.to_i.to_s + image.content_type.gsub('image/', '.')
+    content_image.image = image
+    content_image.save!
+  end
 end
