da92ba9a19c8c0cfdc09ca8beedf197193c711c4
Tiago Santos <santos.tiago@gmail.com>
2019-02-19 13:54:34 +0000

    Changed the size of thumbnails to 800x400

--- app/models/temporary_content_image.rb
+++ app/models/temporary_content_image.rb
@@ -1,5 +1,5 @@
 class TemporaryContentImage < ApplicationRecord
-  has_attached_file :image, styles: { cover: '1280x>', thumb: '110x>' }
+  has_attached_file :image, styles: { cover: '1280x>', thumb: '800x200' }
   validates_attachment_content_type :image, content_type: %r{^image\/.*}
 
   def self.remove_old
