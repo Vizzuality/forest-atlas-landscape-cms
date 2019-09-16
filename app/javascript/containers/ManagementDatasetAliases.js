17324c54c4b4bd5ff534feb942b6538436fa6872
Clément Prod'homme <contact@clementprodhomme.fr>
2019-09-16 16:33:50 +0100

    Update the layout and styles of the form

--- app/javascript/containers/ManagementDatasetAliases.js
+++ app/javascript/containers/ManagementDatasetAliases.js
@@ -7,23 +7,32 @@ const ManagementDatasetAliases = ({ columns }) => (
   <ManagementContainer>
     <div className="l-dataset-creation -aliases">
       <div className="wrapper">
-        {columns.map(column => (
-          <fieldset key={column.name} className="c-inputs-container">
-            <legend>{column.name}</legend>
-            <div className="container">
-              <label htmlFor="dataset-metadata-alias">
-                Alias
-              </label>
-              <input type="text" id="dataset-metadata-alias" name={`columns[${column.name}][alias]`} defaultValue={column.alias || ''} />
-            </div>
-            <div className="container">
-              <label htmlFor="dataset-metadata-description">
-                Description
-              </label>
-              <textarea id="dataset-metadata-description" name={`columns[${column.name}][description]`} defaultValue={column.description || ''} />
-            </div>
-          </fieldset>
-        ))}
+        <div className="c-inputs-container -large">
+          <div className="container -flex">
+            {columns.map((column, index) => (
+              <div key={column.name} className="row">
+                <div className="column column-3">
+                  <label htmlFor={`dataset-metadata-name-${index}`}>
+                    Column name
+                  </label>
+                  <input type="text" id={`dataset-metadata-name-${index}`} name={`columns[${column.name}][name]`} defaultValue={column.name || ''} disabled />
+                </div>
+                <div className="column column-3">
+                  <label htmlFor={`dataset-metadata-alias-${index}`}>
+                    Alias
+                  </label>
+                  <input type="text" id={`dataset-metadata-alias-${index}`} name={`columns[${column.name}][alias]`} defaultValue={column.alias || ''} placeholder="No alias" />
+                </div>
+                <div className="column column-3">
+                  <label htmlFor={`dataset-metadata-description-${index}`}>
+                    Description
+                  </label>
+                  <textarea id={`dataset-metadata-description-${index}`} name={`columns[${column.name}][description]`} defaultValue={column.description || ''} placeholder="Empty description" />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   </ManagementContainer>
