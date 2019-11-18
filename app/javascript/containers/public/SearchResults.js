86fc6fc3b19e36bdb939c73c5711237e4daaba98
Migsar <migsar.navarro@vizzuality.com>
2019-11-18 03:36:07 -0600

    Fix layout and hide search functionality from India template sit… (#376)
    * fix(#169756676): Fix content height for search result for INDIA template.
    
    * fix(#169756676): Hide search tab from header menu.

--- app/javascript/containers/public/SearchResults.js
+++ app/javascript/containers/public/SearchResults.js
@@ -1,19 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-import PublicContainer from "containers/shared/PublicContainer";
-import { Footer } from "components";
-import SearchResults from "components/public/SearchResults";
+import PublicContainer from 'containers/shared/PublicContainer';
+import { Footer } from 'components';
+import SearchResults from 'components/public/SearchResults';
 
 class SearchResultsContainer extends PublicContainer {
   render() {
+    const isIndiaTemplate = this.props.template === 'INDIA';
+
     return (
-      <div>
+      <div className={classnames({ 'fa-page': isIndiaTemplate })}>
         <SearchResults {...this.props} />
         <Footer {...this.store.getState()} />
       </div>
     );
   }
 }
+
+SearchResults.propTypes = {
+  template: PropTypes.string
+};
 
 export default SearchResultsContainer;
