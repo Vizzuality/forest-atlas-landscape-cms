a795332cbe84151daa48e8e556dec32bb08c5d92
Clément Prod'homme <contact@clementprodhomme.fr>
2019-11-26 12:20:51 +0000

    Improve the preview

--- app/javascript/containers/AdminPreviewStatic.js
+++ app/javascript/containers/AdminPreviewStatic.js
@@ -1,151 +1,166 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import Notification from 'components/Notification';
 import Cover from './Cover';
 import HomePage from './HomePage';
 import StaticPage from './StaticPage';
 
-export default function AdminPreviewStatic({ cover, homepage, opencontent }) {
-  const [ state, setState ] = useState({ isHomepage: true });
-  const clickHandler = () => {
-    setState({
-      ...state,
-      isHomepage: !state.isHomepage
-    });
-  };
-  const StaticHeader = () => (
-    <header className="c-header js-header initialized">
-      <div className="mobile-drawer js-mobile-drawer">
-        <div className="wrapper">
-          <ul>
-            <li><a href="/root-level">Root level</a></li>
-            <li>
-              <ul>
-                <li>
-                  <a href="/root-level/level-1">Level 1 Page</a>
-                </li>
-                <li>
-                  <ul>
-                    <li><a href="/root-level/level-1/level-2">Level 2 Page</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li><a href="/map" data-turbolinks="false">Map</a></li>
-            <li>
-              <form method="get" action="/search_results">
-                <div className="c-input-search">
-                  <input type="input" placeholder="Search" name="search" />
-                  <button type="submit">Search</button>
-                </div>
-              </form>
-            </li>
-          </ul>
-          <ul>
-            <li className="notranslate js-language-selector-mobile" />
-          </ul>
-          <ul>
-            <li className="dropdown-item">
-              <div className="profile-image">
-                mi
-              </div>
-              <ul>
-                <li>
-                  <a href="#" className="triggerMySubscriptions">My Subscriptions</a>
-                </li>
-                <li>
-                  <a href="/logout">Logout</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="mobile-menu wrapper">
-        <div className="logo-container" href="/">
-          <a className="logo " href="/">
-            <img alt="Logo" src="/system/site_settings/images/000/001/441/original/rr.PNG?1572967304" />
-          </a>
-        </div>
-        <div className="hamburger-menu js-mobile-menu">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </div>
-      <div className="desktop-menu wrapper">
-        <div className="logo-container">
-          <a className="logo " href="/">
-            <img alt="Logo" src="/system/site_settings/images/000/001/441/original/rr.PNG?1572967304" />
-          </a>
-        </div>
-        <div className="menu-container">
-          <div className="menu">
+export default function AdminPreviewStatic({ siteSettings, homepage, opencontent }) {
+  const [isHomepage, setIsHomepage] = useState(true);
+
+  const onClickHomepage = useCallback(() => setIsHomepage(true), [setIsHomepage]);
+  const onClickOpenContent = useCallback(() => setIsHomepage(false), [setIsHomepage]);
+
+  const clickHandler = useCallback(() => {
+    setIsHomepage(!isHomepage);
+  }, [isHomepage, setIsHomepage]);
+
+  const StaticHeader = () => {
+    const countryColorsSetting = siteSettings.find(setting => setting.name === 'header-country-colours');
+    const countryColors = countryColorsSetting ? countryColorsSetting.value.split(' ') : [];
+    return (
+      <header className="c-header js-header initialized">
+        <div className="mobile-drawer js-mobile-drawer">
+          <div className="wrapper">
             <ul>
-              <li className="dropdown-item -active">
-                <a href="/root-level">Root level</a>
+              <li className={classnames({ 'button-item': true, '-active': !isHomepage })}>
+                <button type="button" onClick={clickHandler}>Open Content</button>
+              </li>
+              <li className="dropdown-item">
+                <a>Sample menu</a>
                 <ul>
                   <li>
-                  </li>
-                  <li>
-                    <a href="/root-level/level-1">Level 1 Page</a>
+                    <a>Level 1 Page</a>
                   </li>
                 </ul>
               </li>
-              <li className="">
-                <a href="#" data-turbolinks="false" onClick={clickHandler}>Open Content</a>
+              <li>
+                <form method="get" action="/search_results">
+                  <div className="c-input-search">
+                    <input type="input" placeholder="Search" name="search" />
+                    <button type="submit">Search</button>
+                  </div>
+                </form>
               </li>
-              <li className="dropdown-item search js-search">
-                <button type="button" className="search-button js-search-button" href="#">Search</button>
-                <ul>
-                  <li>
-                    <form method="get" action="/search_results">
-                      <div className="c-input-search">
-                        <input type="input" placeholder="Search" name="search" />
-                        <button type="submit">Search</button>
-                      </div>
-                    </form>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <ul>
-              <li className="notranslate js-language-selector" />
             </ul>
           </div>
-          <div className="sub-menu">
-            <div className="content">
-              <ul className="breadcrumbs">
-                <li><a href="/">Home</a></li>
-                <li><a href="/root-level">Root level</a></li>
-                <li><a href="/root-level/level-1">Level 1 Page</a></li>
-                <li><a href="/root-level/level-1/level-2">Level 2 Page</a></li>
+        </div>
+        <div className="mobile-menu wrapper">
+          <div className="logo-container">
+            <button type="button" className="logo" onClick={onClickHomepage}>
+              Logo
+            </button>
+          </div>
+          <div className="hamburger-menu js-mobile-menu">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className="desktop-menu wrapper">
+          <div className="logo-container">
+            <button type="button" className="logo" onClick={onClickHomepage}>
+              Logo
+            </button>
+          </div>
+          <div className="menu-container">
+            <div className="menu">
+              <ul>
+                <li className={classnames({ 'button-item': true, '-active': !isHomepage })}>
+                  <button type="button" onClick={clickHandler}>Open Content</button>
+                </li>
+                <li className="dropdown-item">
+                  <a>Sample menu</a>
+                  <ul>
+                    <li>
+                      <a>Level 1 Page</a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="dropdown-item search js-search">
+                  <button type="button" className="search-button js-search-button">Search</button>
+                </li>
               </ul>
-              <ul className="sections">
-                <ul>
-                  <li className="-active"><a href="/root-level/level-1/level-2">Level 2 Page</a></li>
+              <ul />
+            </div>
+            <div className="sub-menu">
+              <div className="content">
+                <ul className="breadcrumbs">
+                  {!isHomepage && (
+                    <>
+                      <li className="button-item">
+                        <button type="button" onClick={onClickHomepage}>Home</button>
+                      </li>
+                      &nbsp;
+                      <li className="button-item">
+                        <button type="button" onClick={onClickOpenContent}>Open Content</button>
+                      </li>
+                    </>
+                  )}
                 </ul>
-              </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="flag-border">
-        <div style={{backgroundColor: '#000'}} />
-      </div>
-    </header>
-  );
+        <div className="flag-border">
+          {countryColors.map(color => <div style={{ backgroundColor: color }} />)}
+        </div>
+      </header>
+    );
+  };
 
   return (
-    <>
+    <div className="l-template-preview">
+      <Notification
+        type="warning"
+        content="The content of this preview is not representative of the real content of the site."
+        additionalContent={(
+          <>
+            Click here to preview the{' '}
+            <button
+              type="button"
+              className={classnames({
+                'c-button': true,
+                '-monochrome': true,
+                '-mini': true,
+                '-outline': !isHomepage
+              })}
+              aria-pressed={isHomepage}
+              onClick={onClickHomepage}
+            >
+              Homepage
+            </button>{' '}
+            or an{' '}
+            <button
+              type="button"
+              className={classnames({
+                'c-button': true,
+                '-monochrome': true,
+                '-mini': true,
+                '-outline': isHomepage
+              })}
+              aria-pressed={!isHomepage}
+              onClick={onClickOpenContent}
+            >
+              Open Content
+            </button>{' '}
+            page.
+          </>
+        )}
+        closeable={false}
+        onClose={() => {}}
+      />
       <StaticHeader />
-      <Cover {...cover} />
-      {state.isHomepage ? <HomePage {...homepage} /> : <StaticPage {...opencontent} />}
-    </>
+      <Cover {...(isHomepage ? homepage.cover : opencontent.cover)} />
+      {isHomepage ? <HomePage {...homepage} /> : <StaticPage {...opencontent} />}
+    </div>
   );
 }
 
 AdminPreviewStatic.propTypes = {
-  cover: PropTypes.shape({}).isRequired,
+  siteSettings: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   homepage: PropTypes.shape({}).isRequired,
   opencontent: PropTypes.shape({}).isRequired,
 };
