Vizzuality/forest-atlas-landscape-cms

commit b10a76c55bd90bc02f470e066ab98b71477eb759

Fix a bug where the wrong metadata would be used

app/javascript/components/shared/Dashboard/dashboard.helpers.js
@@ -18,7 +18,7 @@ const isEmptyMetadata = (metadata) => {
   ];
 
   return keys.every((key) => {
-    const value = get(metadata, key);
+    const value = get(metadata.attributes, key);
     return value === '' || value === undefined || value === null;
   });
 };

commit 1785c3b17250b82e3ea37b4b425f479a81e76007

Fix a regression from b10a76c

app/javascript/components/shared/Dashboard/dashboard.helpers.js
@@ -1,6 +1,10 @@
 import get from 'lodash/get';
 
 const isEmptyMetadata = (metadata) => {
+  if (!metadata) {
+    return true;
+  }
+
   const keys = [
     'description',
     'citation',