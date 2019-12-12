3ed0a1824613aeef8603bf2f1a39501d5af1524f
Clément Prod'homme <contact@clementprodhomme.fr>
2019-12-12 17:30:51 +0000

    Re-use the preview button to open the preview page

--- app/javascript/containers/AdminPreviewButton.js
+++ app/javascript/containers/AdminPreviewButton.js
@@ -3,98 +3,174 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import queryString from 'query-string';
 
-const consumer = window.ActionCable.createConsumer();
+const CONSUMER = window.ActionCable.createConsumer();
 
-// Not really React way but nothing can be done while the form is not in a component.
-function getSiteSettings() {
-  // We could use just the selector if naming followed a regular rule.
-  const fields = [
-    { name: 'color', selector: '#accent-color_id' },
-    { name: 'content_width', selector: '#content_width' },
-    { name: 'content_font', selector: '#content_font' },
-    { name: 'heading_font', selector: '#heading_font' },
-    { name: 'cover_size', selector: '#cover_size' },
-    { name: 'cover_text_alignment', selector: '#cover_text_alignment' },
-    { name: 'header_separators', selector: '#header_separators' },
-    { name: 'header_background', selector: '#header_background' },
-    { name: 'header_transparency', selector: '#header_transparency' },
-    { name: 'header_login_enabled', selector: '#header_login_enabled' },
-    { name: 'footer_background', selector: '#footer_background' },
-    { name: 'footer_text_color', selector: '#footer_text_color' },
-    { name: 'footer_links_color', selector: '#footer-links-color' }
-  ];
+const SETTINGS_DICT = {
+  color: { selector: '#accent-color_id', multiple: false },
+  content_width: { selector: '#content_width', multiple: false },
+  content_font: { selector: '#content_font', multiple: false },
+  heading_font: { selector: '#heading_font', multiple: false },
+  cover_size: { selector: '#cover_size', multiple: false },
+  cover_text_alignment: { selector: '#cover_text_alignment', multiple: false },
+  header_separators: { selector: '#header_separators', multiple: false },
+  header_background: { selector: '#header_background', multiple: false },
+  header_transparency: { selector: '#header_transparency', multiple: false },
+  header_login_enabled: { selector: '#header_login_enabled', multiple: false },
+  'header-country-colours': {
+    selector: '.country-colors-container input[type="color"]',
+    multiple: true
+  },
+  footer_background: { selector: '#footer_background', multiple: false },
+  footer_text_color: { selector: '#footer_text_color', multiple: false },
+  footer_links_color: { selector: '#footer-links-color', multiple: false }
+};
 
-  const values = fields.reduce((acc, field) => ({
-    ...acc,
-    [field.name]: document.querySelector(field.selector).value
-  }), {});
+const getSettingsValue = () => {
+  return Object.keys(SETTINGS_DICT).reduce((res, setting) => {
+    const element = SETTINGS_DICT[setting].multiple
+      ? document.querySelectorAll(SETTINGS_DICT[setting].selector)
+      : document.querySelector(SETTINGS_DICT[setting].selector);
 
-  const headerCountryColors = Array.from(document.querySelectorAll('.country-colors-container input[type="color"]'))
-    .map(countryColor => countryColor.value).join(' ');
+    if (!element) {
+      return res;
+    }
 
-  return { ...values, 'header-country-colours': headerCountryColors };
-}
+    return {
+      ...res,
+      [setting]: SETTINGS_DICT[setting].multiple
+        ? [...element].map(({ value }) => value).join(' ')
+        : element.value
+    };
+  }, {});
+};
 
-export default function AdminPreviewButton({ className, text, slug }) {
-  const [isLoading, setIsLoading] = useState(false);
+const setSettingsListener = listener => {
+  Object.keys(SETTINGS_DICT).forEach(setting => {
+    const selector = SETTINGS_DICT[setting].selector;
+    const isMulti = SETTINGS_DICT[setting].multiple;
 
+    if (!isMulti) {
+      const element = document.querySelector(selector);
+      element.addEventListener('change', listener);
+    } else {
+      const elements = document.querySelectorAll(selector);
+      elements.forEach(element => {
+        element.addEventListener('change', listener);
+      });
+    }
+  });
+};
+
+const removeSettingsListener = listener => {
+  Object.keys(SETTINGS_DICT).forEach(setting => {
+    const selector = SETTINGS_DICT[setting].selector;
+    const isMulti = SETTINGS_DICT[setting].multiple;
+
+    if (!isMulti) {
+      const element = document.querySelector(selector);
+      element.removeEventListener('change', listener);
+    } else {
+      const elements = document.querySelectorAll(selector);
+      elements.forEach(element => {
+        element.removeEventListener('change', listener);
+      });
+    }
+  });
+};
+
+export default function AdminPreviewButton({ className, slug }) {
+  // State machine without constrained transitions
+  // - dirty: the stylesheet needs to be re-generated
+  // - loading: the stylesheet is being generated
+  // - finished: the stylesheet has been generated
+  const [state, setState] = useState('dirty');
+  const [previewUrl, setPreviewUrl] = useState(null);
+  const setDirtyState = useCallback(() => {
+    setState('dirty');
+    setPreviewUrl(null);
+  }, [setState, setPreviewUrl]);
+  const setLoadingState = useCallback(() => setState('loading'), [setState]);
+  const setFinishedState = useCallback(
+    url => {
+      setState('finished');
+      setPreviewUrl(url);
+    },
+    [setState, setPreviewUrl]
+  );
+
+  const onReceiveData = data => {
+    const siteSettings = JSON.parse(data.site_settings);
+    const host = `${window.location.origin}/admin/sites/${slug}/preview`;
+    const queryParams = {
+      'header-country-colours': siteSettings['header-country-colours'].split(
+        ' '
+      ),
+      header_login_enabled: siteSettings.header_login_enabled
+    };
+    const url = `${host}?${queryString.stringify(queryParams, {
+      arrayFormat: 'bracket'
+    })}`;
+
+    setFinishedState(url);
+  };
+
+  const onClickButton = useCallback(() => {
+    $.get(
+      `${window.location.origin}/admin/sites/${slug}/preview/compile`,
+      { site_settings: getSettingsValue() },
+      () => setLoadingState()
+    );
+  }, [setLoadingState]);
+
+  // Set the listeners to switch to the dirty state
   useEffect(() => {
-    consumer.subscriptions.create('PreviewChannel', {
+    setSettingsListener(setDirtyState);
+
+    return () => {
+      removeSettingsListener(setDirtyState);
+    };
+  }, [setDirtyState]);
+
+  // Create the websocket connection
+  useEffect(() => {
+    CONSUMER.subscriptions.create('PreviewChannel', {
       connected: () => {},
       disconnected: () => {},
-      received: (data) => {
-        const siteSettings = JSON.parse(data.site_settings);
-        const host = `${window.location.origin}/admin/sites/${slug}/preview`;
-        const queryParams = {
-          'header-country-colours': siteSettings['header-country-colours'].split(' '),
-          header_login_enabled: siteSettings.header_login_enabled
-        };
-        const url = `${host}?${queryString.stringify(queryParams, {arrayFormat: 'bracket'})}`;
-
-        setIsLoading(false);
-
-        // Open preview page
-        window.open(url, '_blank');
-      }
+      received: onReceiveData
     });
 
     return () => {};
   }, []);
 
-  const compileStyleSheet = () => {
-    $.get(
-      `${window.location.origin}/admin/sites/${slug}/preview/compile`,
-      { site_settings: getSiteSettings() },
-      () => setIsLoading(true),
+  if (state === 'finished') {
+    return (
+      <a href={previewUrl} target="_blank" className={classnames(className)}>
+        See preview
+      </a>
     );
-  };
-
-  const onClickButton = useCallback(() => {
-    if (!isLoading) {
-      compileStyleSheet();
-    }
-  }, [isLoading]);
+  }
 
   return (
     <button
       type="button"
       className={classnames(className)}
       onClick={onClickButton}
-      disabled={isLoading}
+      disabled={state === 'loading'}
     >
-      {isLoading ? <div className="c-loading-spinner -inline -small" /> : text}
+      {state === 'dirty' && 'Update preview'}
+      {state === 'loading' && (
+        <div className="c-loading-spinner -inline -small" />
+      )}
     </button>
   );
 }
 
 AdminPreviewButton.propTypes = {
-  text: PropTypes.string,
   className: PropTypes.string,
   slug: PropTypes.string
 };
 
 AdminPreviewButton.defaultProps = {
-  text: '',
   className: '',
   slug: null
 };
