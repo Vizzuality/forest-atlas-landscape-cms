Vizzuality/forest-atlas-landscape-cms

commit 2bb7e12e33d0b34c4974c2f191675e5f27295139

Add option to configure the height of the widgets

app/javascript/components/shared/Wysiwyg/blocks/WidgetBlock.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import { VegaChart } from 'widget-editor';
 import { Promise } from 'es6-promise';
 
-import { isVegaWidget, getVegaWidgetQueryParams, getDatasetDownloadUrls, getSqlFilters } from 'helpers/api';
+import {
+  isVegaWidget,
+  getVegaWidgetQueryParams,
+  getDatasetDownloadUrls,
+  getSqlFilters
+} from 'helpers/api';
 import Icon from 'components/icon';
 
 // /widget_data.json?widget_id=
@@ -15,7 +20,11 @@ class WidgetBlock extends React.Component {
   static async getDatasetProvider(datasetId) {
     const query = `${ENV.API_URL}/dataset/${datasetId}`;
     const data = await fetch(query).then(res => res.json());
-    const { data: { attributes: { provider } } } = data;
+    const {
+      data: {
+        attributes: { provider }
+      }
+    } = data;
     return provider;
   }
 
@@ -29,12 +38,18 @@ class WidgetBlock extends React.Component {
       return {};
     }
 
-    const datasetProvider = await WidgetBlock.getDatasetProvider(widget.dataset);
-    const { filters, limit } = getVegaWidgetQueryParams({ widgetConfig: widget.visualization });
+    const datasetProvider = await WidgetBlock.getDatasetProvider(
+      widget.dataset
+    );
+    const { filters, limit } = getVegaWidgetQueryParams({
+      widgetConfig: widget.visualization
+    });
 
     const serializedFilters = getSqlFilters(filters, datasetProvider);
 
-    const sqlQuery = `SELECT * FROM data ${serializedFilters.length ? `WHERE ${serializedFilters}` : ''}`;
+    const sqlQuery = `SELECT * FROM data ${
+      serializedFilters.length ? `WHERE ${serializedFilters}` : ''
+    }`;
 
     return getDatasetDownloadUrls(widget.dataset, datasetProvider, sqlQuery);
   }
@@ -51,50 +66,88 @@ class WidgetBlock extends React.Component {
 
   componentWillMount() {
     const { item } = this.props;
-    this.getChart(item.content.widgetId)
-      .then(async () => {
-        this.setState({ downloadUrls: await WidgetBlock.getDownloadUrls(this.state.widget) });
+    this.getChart(item.content.widgetId).then(async () => {
+      this.setState({
+        downloadUrls: await WidgetBlock.getDownloadUrls(this.state.widget)
       });
+    });
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     // This fixes a bug where the widgets would reload when hovering them
-    return this.state !== nextState
-      || this.props.item.content.widgetId !== nextProps.item.content.widgetId;
+    return (
+      this.state !== nextState ||
+      this.props.item.content.widgetId !== nextProps.item.content.widgetId ||
+      this.props.item.content.height !== nextProps.item.content.height
+    );
   }
 
   getChart(widgetId) {
-    return fetch(`${window.location.origin}/widget_data.json?widget_id=${widgetId}`).then((res) => {
-      return res.json();
-    }).then((w) => new Promise((resolve) => {
-      const widget = w;
-      if (widget.visualization && widget.visualization.width !== undefined) delete widget.visualization.width;
-      if (widget.visualization && widget.visualization.height !== undefined) delete widget.visualization.height;
+    return fetch(
+      `${window.location.origin}/widget_data.json?widget_id=${widgetId}`
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(
+        w =>
+          new Promise(resolve => {
+            const widget = w;
+            if (
+              widget.visualization &&
+              widget.visualization.width !== undefined
+            )
+              delete widget.visualization.width;
+            if (
+              widget.visualization &&
+              widget.visualization.height !== undefined
+            )
+              delete widget.visualization.height;
 
-      this.setState({
-        loading: false,
-        widget
-      }, resolve);
-    }));
+            this.setState(
+              {
+                loading: false,
+                widget
+              },
+              resolve
+            );
+          })
+      );
   }
 
   render() {
+    const { readOnly, item, onChange } = this.props;
+    const { height } = item.content;
     const { loading, widget, downloadUrls } = this.state;
 
-    if (loading || !widget.visualization) { return null; }
+    if (loading || !widget.visualization) {
+      return null;
+    }
 
     return (
       <Fragment>
         <div className="c-we-chart-title">{widget.name}</div>
-        <VegaChart
-          data={widget.visualization}
-          reloadOnResize
-        />
-        {widget.metadata && !!widget.metadata.length && widget.metadata[0].attributes.info && (
-          <div className="c-we-chart-caption">
-            {widget.metadata[0].attributes.info.caption}
-          </div>
-        )}
+        <div
+          className="c-we-chart-container"
+          // We choose min-height instead of height because custom widget might define a higher
+          // height
+          // Anyway, we don't want widget shorter than 250px
+          style={{ 'min-height': `${Math.max(height, 250) || 250}px` }}
+        >
+          <VegaChart
+            // The key here is used to make sure the widget is rerendered when its height is changed
+            key={height}
+            data={widget.visualization}
+            reloadOnResize
+          />
+        </div>
+        {widget.metadata &&
+          !!widget.metadata.length &&
+          widget.metadata[0].attributes.info && (
+            <div className="c-we-chart-caption">
+              {widget.metadata[0].attributes.info.caption}
+            </div>
+          )}
         <div className="c-we-chart-download">
           {downloadUrls.csv && (
             <a
@@ -107,6 +160,39 @@ class WidgetBlock extends React.Component {
             </a>
           )}
         </div>
+        {!readOnly && (
+          <div className="fa-wysiwyg-configuration">
+            <header>
+              <span>Configuration</span>
+            </header>
+            <main>
+              <label>
+                Height (250px minimum)
+                <input
+                  type="number"
+                  className="fa-wysiwyg-file__preview--captionInput"
+                  placeholder="Height in pixels"
+                  defaultValue={height || 250}
+                  min={250}
+                  onChange={e =>
+                    onChange({
+                      content: {
+                        ...item.content,
+                        height: Number.isInteger(+e.target.value)
+                          ? Math.max(+e.target.value, 250) // Minimum height should stay 250px
+                          : height
+                      }
+                    })
+                  }
+                />
+              </label>
+              <small>
+                Please note that if a custom widget has its height defined in
+                the Vega specification, it won't be overwritten.
+              </small>
+            </main>
+          </div>
+        )}
       </Fragment>
     );
   }

app/assets/stylesheets/components/shared/c-vizz-wysiwyg.scss
@@ -111,8 +111,16 @@
         }
       }
     }
+
+    &.-widget {
+      .fa-wysiwyg-configuration {
+        margin-top: 10px;
+      }
+    }
     
     .fa-wysiwyg-configuration {
+      width: 100%;
+      min-height: 130px; // Needed for the header
       display: flex;
       align-items: stretch;
       border: 1px solid $color-6;
@@ -143,6 +151,30 @@
 
         > .no-margin-bottom {
           margin-bottom: 0;
+        }
+
+        label {
+          display: block;
+        }
+
+        input {
+          display: block;
+          padding: 10px;
+          margin-top: 5px;
+          font-size: $font-size-small;
+          border: 1px solid $color-9;
+          color: $color-4;
+        }
+
+        textarea {
+          display: block;
+          width: 100%;
+          min-height: 30px;
+          padding: 10px;
+          margin-top: 5px;
+          font-size: $font-size-small;
+          border: 1px solid $color-9;
+          color: $color-4;
         }
       }
     }
@@ -441,11 +473,29 @@
       }
     }
   }
-
-  .c-we-chart,
-  .c-we-chart .chart {
-    min-height: 250px;
+  
+  // Here we use flex boxes instead of just setting the height to 100% because the top container
+  // (.c-we-chart-container) has a min-height defined in JS
+  // Because of that, the children can't use height: 100%;
+  .c-we-chart-container {
+    display: flex;
+    flex-direction: column;
     width: 100%;
+    min-height: 250px;
+
+    .c-we-chart {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      flex-basis: 100%;
+      flex-grow: 1;
+
+      .chart {
+        width: 100%;
+        flex-basis: 100%;
+        flex-grow: 1;
+      }
+    }
   }
 
   .c-we-chart-title {
@@ -520,31 +570,6 @@
 
       border: 1px solid transparent;
       cursor: pointer;
-    }
-  }
-
-  &__preview {
-    &--caption {
-      display: block;
-      font-size: $font-size-small;
-      padding: 5px;
-      color: $color-4;
-    }
-
-    &--captionInput {
-      display: block;
-
-      width: 100%;
-      min-height: 30px;
-
-      padding: 10px;
-      margin: 20px 0 0 0;
-
-      font-size: $font-size-small;
-      border: 1px solid $color-9;
-      color: $color-4;
-
-      resize: none;
     }
   }
 }

app/javascript/components/shared/Wysiwyg/blocks/ImagePreview.js
@@ -24,7 +24,6 @@ class ImagePreview extends PureComponent {
               <label>
                 Caption
                 <textarea
-                  className="fa-wysiwyg-file__preview--captionInput"
                   placeholder="Add caption"
                   defaultValue={caption}
                   onChange={e =>
@@ -41,7 +40,6 @@ class ImagePreview extends PureComponent {
               <label className="no-margin-bottom">
                 Alternative text
                 <textarea
-                  className="fa-wysiwyg-file__preview--captionInput"
                   placeholder="Add alternative text"
                   defaultValue={alternativeText}
                   onChange={e =>

commit 664aacc7f0f5d956903a59212404e9b754551546

Fix an unrelated React error introduced in 2bb7e12

app/javascript/components/shared/Wysiwyg/blocks/WidgetBlock.js
@@ -133,7 +133,7 @@ class WidgetBlock extends React.Component {
           // We choose min-height instead of height because custom widget might define a higher
           // height
           // Anyway, we don't want widget shorter than 250px
-          style={{ 'min-height': `${Math.max(height, 250) || 250}px` }}
+          style={{ minHeight: `${Math.max(height, 250) || 250}px` }}
         >
           <VegaChart
             // The key here is used to make sure the widget is rerendered when its height is changed