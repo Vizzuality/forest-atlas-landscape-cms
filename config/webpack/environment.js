baa045a9fe82a6b7d07ef6aa358831d8adc2e2b5
Clément Prod'homme <contact@clementprodhomme.fr>
2020-08-14 13:16:03 +0100

    Remove unneeded dependency used in v1

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,18 +3,7 @@ require('dotenv').config({ silent: true });
 
 const vegaPackage = require('vega/package.json');
 const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { environment } = require('@rails/webpacker');
-
-environment.plugins.prepend(
-  'Provide',
-  new CopyWebpackPlugin([
-    {
-      from: 'node_modules/widget-editor/dist/images',
-      to: 'images/'
-    }
-  ])
-);
 
 environment.plugins.append('definePlugin', new webpack.DefinePlugin({
   'ENV.API_URL': JSON.stringify(process.env.API_URL),
