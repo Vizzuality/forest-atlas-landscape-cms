53364cfc24ec00e27c15e6f321cec7c5e494b392
Clément Prod'homme <contact@clementprodhomme.fr>
2020-08-17 10:03:35 +0100

    Fix bug breaking Open Content pages (empty)

--- app/javascript/containers/StaticPage.js
+++ app/javascript/containers/StaticPage.js
@@ -3,8 +3,10 @@ import React from 'react';
 import PublicContainer from 'containers/shared/PublicContainer';
 import StaticPages from 'pages/static';
 
-export default class StaticPage extends PublicContainer {
-  render() {
-    return <StaticPages {...this.props} store={this.store} />;
-  }
-}
+const StaticPageContainer = props => (
+  <PublicContainer {...props}>
+    <StaticPages />
+  </PublicContainer>
+);
+
+export default StaticPageContainer;
